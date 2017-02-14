f22d6241a40afd12e49c3157c310bfe0ab2eb091
Anton Chuah <anthony.w.chuah@gmail.com>
2017-02-14 22:57:52 +0000

    Testing

--- Basic/arrays.java
+++ Basic/arrays.java
@@ -1,38 +1,53 @@
+import java.lang.Integer;
+
 class ArrayTest {
+    public ArrayTest(int num) {
+	testArray = new Integer[num];
+    }
     public static void main (String [] args) {
-	int [] test1 = {1, 5, 2, 0, 3, 0, 6};
-	int [] test2;
-	test2 = new int [3];
-	test2[0] = 2;
-	test2[2] = 1;
-	System.out.println("First array test1 {1, 5, 2, 0, 3, 0, 6} length: " + test1.length);
-	System.out.println("Second array test2 length: " + test2.length);
-	// I can now re-allocate new test2 of different size!
-	test2 = new int [5];
-	test2[0] = 1;
-	System.out.println("Reallocated array test2 to new length of 5, verify: " + test2.length);
-	// Now test the NonZeros function.
-	int [] strip1 = NonZeros(test1);
-	int [] strip2 = NonZeros(test2);
-	// After stripping zeroes, we should have lengths of 5 and 1.
-	System.out.println("Lengths should be 5 and 1, verify: " + strip1.length + " " + strip2.length);
-	// Now demonstrate copying of arrays.
-	int [] X, Y;
-	X = new int [10];
-	X[0] = 5; X[3] = 3; X[6] = 10; X[8] = 19;
-	Y = new int [5];
-	System.arraycopy(A, 0, B, 0, 5); // Copies first 5 values from A to B.
-	System.arraycopy(A, 9, B, 4, 1); // Copies last element from A into last element of B.
-	// An array declaration is really making a POINTER. So when you assign raw arrays you are
-	// really doing a pointer assignment. Example:
-	int [] A1 = new int [3];
-	int [] B1 = new int [2];
-	A[0] = 5; // A --> {5, 0, 0}
-	B = A; // A AND B --> {5, 0, 0}
-	B[0] = 2; // Means that now A[0] is also 2! Exactly like a pointer.
+	// int [] test1 = {1, 5, 2, 0, 3, 0, 6};
+	// int [] test2;
+	// test2 = new int [3];
+	// test2[0] = 2;
+	// test2[2] = 1;
+	// System.out.println("First array test1 {1, 5, 2, 0, 3, 0, 6} length: " + test1.length);
+	// System.out.println("Second array test2 length: " + test2.length);
+	// // I can now re-allocate new test2 of different size!
+	// test2 = new int [5];
+	// test2[0] = 1;
+	// System.out.println("Reallocated array test2 to new length of 5, verify: " + test2.length);
+	// // Now test the NonZeros function.
+	// int [] strip1 = NonZeros(test1);
+	// int [] strip2 = NonZeros(test2);
+	// // After stripping zeroes, we should have lengths of 5 and 1.
+	// System.out.println("Lengths should be 5 and 1, verify: " + strip1.length + " " + strip2.length);
+	// // Now demonstrate copying of arrays.
+	// int [] X, Y;
+	// X = new int [10];
+	// X[0] = 5; X[3] = 3; X[6] = 10; X[8] = 19;
+	// Y = new int [5];
+	// System.arraycopy(A, 0, B, 0, 5); // Copies first 5 values from A to B.
+	// System.arraycopy(A, 9, B, 4, 1); // Copies last element from A into last element of B.
+	// // An array declaration is really making a POINTER. So when you assign raw arrays you are
+	// // really doing a pointer assignment. Example:
+	// int [] A1 = new int [3];
+	// int [] B1 = new int [2];
+	// A[0] = 5; // A --> {5, 0, 0}
+	// B = A; // A AND B --> {5, 0, 0}
+	// B[0] = 2; // Means that now A[0] is also 2! Exactly like a pointer.
 	// In Java, all parameters are passed by value, but for arrays and classes, actually it is
 	// passing a pointer.
 	// Copying objects in C++ requires copy constructor. In Java, you use clone.
+	ArrayTest aTest = new ArrayTest(10);
+	Integer[] testInput = new Integer[10];
+	for (int i = 0; i < testInput.length; ++i) {
+	    testInput[i] = i;
+	}
+	aTest.populateTestArray(testInput);
+	System.out.println("Testing populateTestArray: should see 1 to 10: array size is: " + aTest.testArraySize());
+	for (int i = 0; i < aTest.testArraySize(); ++i) {
+	    System.out.print(aTest.testArray[i] + " ");
+	}
     }
     public static int[] NonZeros (int [] A) {
 	int count_nonzero = 0;
@@ -52,4 +67,11 @@ class ArrayTest {
 	}
 	return result;
     }
+    public void populateTestArray(Integer[] inputIntArray) {
+	testArray = inputIntArray;
+    }
+    public int testArraySize() {
+	return testArray.length;
+    }
+    private Integer[] testArray;
 }
